ddda6683c99bc6613ead3bc190fd4d93012884d7
gdileep <gdileep@10.0.0.147>
2018-03-27 23:03:10 -0400

    Fixed Navbar

--- lib/header/head.php
+++ lib/header/head.php
@@ -10,11 +10,9 @@
 <meta name="Project for Hacking Health" content="The HTML">
 <meta name="Dileep Gunda" content="The Green Form">
 <noscript>Your browser does not support JavaScript!</noscript>
-<link rel="stylesheet" href="..\..\lib\css\bootstrap.min.css">
+<link rel="stylesheet" type="text/css"
+	href="..\lib\css\bootstrap.min.css">
 <script src="..\lib\js\jquery.min.js"></script>
 <script src="..\lib\js\bootstrap.min.js"></script>
-<!--   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
-<!--   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
-<!--   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 </head>
 <!--/.Header-->
