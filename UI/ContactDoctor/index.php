5501d06a34234abd1a0718416ffb41ec4544f60b
gdileep <gdileep@10.0.0.147>
2018-04-12 10:58:08 -0400

    Contact Page updated

--- UI/ContactDoctor/index.php
+++ UI/ContactDoctor/index.php
@@ -1,16 +1,47 @@
 <?php
 // echo "hello";
 include '../../config/rootconfig.php';
-include $root.'/lib/header/head.php';
+include $root . '/lib/header/head.php';
+echo "<style>";
+include $root . '/lib/css/contactdoctor.css';
+echo "</style>";
+echo "</head>";
 echo "<body>";
 include $root . '/lib/Navigation/navigation.php';
 ?>
  <?php
-
-echo "Contact Page";
+// echo "Contact Page";
 ?>
- 
- <?php
+<div id="contactbck"
+	style="text-align: center; margin: auto; color: #888888; font-size: 20px; font-weight: bold">
+	<table id="contacttbl">
+		<tr>
+			<td><img alt="Contact A Medical Cannabis Doctor"
+				src="/img/contact.png" /></td>
+			<td><span style="color: white"><h1>Contact A Medical Cannabis Doctor</h1></span></td>
+		</tr>
+	</table>
+
+
+	<table>
+		<tr>
+			<td><img alt="The Green Pharm" src="/img/chat.png" /></td>
+		</tr>
+		<tr>
+			<td><span style="color: white">Chat with a doctor! </span></td>
+		</tr>
+		<tr>
+			<td><img alt="The Green Pharm" src="/img/call.png" /></td>
+		</tr>
+		<tr>
+			<td><span style="color: white">Call a doctor!
+! </span></td>
+		</tr>
+	</table>
+
+</div>
+
+<?php
 include $root . '/lib/footer/footer.php';
 echo "</body>";
 ?>
